c395cc9eac3bbaeb3d0076b0cedd3e029fece79c
John Parent <parenj@rpi.edu>
2019-11-18 15:54:40 -0500

    bug fixes

--- scripts/servoHost.py
+++ scripts/servoHost.py
@@ -92,6 +92,7 @@ def main():
         RRN.RegisterServiceType(service_def)
         RRN.RegisterService("Servo","servo.Servo",obj)
         print(RRN.GetRegisteredServiceTypes())
+        print(RRN.IsServiceTypeRegistered('servo'))
         raw_input("Server_started,_press_enter_to_quit...")
 
         obj.Close()
