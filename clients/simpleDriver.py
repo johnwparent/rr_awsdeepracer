a7e13f87757ee65a7e7147d17c2522ef9b8aa176
John Parent <johnwparent@gmail.com>
2019-11-18 17:31:35 -0500

    servo functionality

--- clients/simpleDriver.py
+++ clients/simpleDriver.py
@@ -16,8 +16,9 @@ if __name__ == '__main__':
     print(RRN.IsServiceTypeRegistered('servo'))
     servo_ctrl = RRN.ConnectService(url)
     servo_ctrl.testing()
-    servo_ctrl.Go()
-    servo_ctrl.Drive(0.5,0)
-    time.sleep(1)
+    servo_ctrl.Drive(0.8,0.1)
+    time.sleep(2)
+    servo_ctrl.Drive(0.4,0.1)
+    time.sleep(4)
     servo_ctrl.Stop()
     
