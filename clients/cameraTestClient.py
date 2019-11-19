743b46caebc83a9b6f9cbbf2158b60557c203fa3
John Parent <parenj@rpi.edu>
2019-11-19 15:25:52 -0500

    testing camera

--- clients/cameraTestClient.py
+++ clients/cameraTestClient.py
@@ -4,13 +4,15 @@ import numpy as np
 import cv2
 import sys
 import threading
-from camera_calibration import CameraCalibration as cc
+from camera_calibration import CameraCalibration
 
 
 if __name__ == '__main__':
     url = 'rr+tcp://localhost:'+sys.argv[1]+'?service=AWSCamera'
     print(url)
     cam_ctrl = RRN.ConnectService(url)
+    cam_ctrl.startCamera()
+    cc = CameraCalibration()
     image1 = cc.reformat(cam_ctrl.getCurrentImage())
     image2 = cc.reformat(cam_ctrl.getCurrentImage())
     cv2.imshow("1st test",image1)
