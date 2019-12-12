dde60923a981dd58ac8f77ad1c72170e4af42d56
John Parent <parenj@rpi.edu>
2019-12-12 13:55:13 -0500

    deadrace condition fix

--- scripts/cameraHost.py
+++ scripts/cameraHost.py
@@ -51,14 +51,13 @@ class DeepRacerCamera_impl(object):
         print ("Initializing cam Node")
         rospy.init_node('aws_cams', anonymous = True)
         
-        
+        self._imagestream_endpoints = {}
+        self._imagestream_endpoints_lock=threading.RLock()
         # Lock for multithreading
         self._lock = threading.RLock()
         self._camera_sub = False
         # for image pipe
         self._imagestream = None
-        self._imagestream_endpoints = dict()
-        self._imagestream_endpoints_lock = threading.RLock()
         
        
     def set_image_struct(self):
@@ -89,20 +88,8 @@ class DeepRacerCamera_impl(object):
                 self._image.step = camdata.step
                 self._image.encoding = camdata.encoding
                 self._image.is_bigendian = camdata.is_bigendian
-
-        with self._imagestream_endpoints_lock:
-            # Send to pipe endpoints
-            for ep in self._imagestream_endpoints:
-                dict_ep = self._imagestream_endpoints[ep]
-                # Loop through indices in nested dict
-                for ind in dict_ep:
-                    # Attempt to send frame to connected endpoint
-                    try:
-                        pipe_ep=dict_ep[ind]
-                        pipe_ep.SendPacket(self._image)
-                    except:
-                        # on error, assume pipe has been closed
-                        self.ImageStream_pipeclosed(pipe_ep)
+                self._imagestream_broadcaster.SendPacket(self._image,lambda: None)
+        
     def getCurrentImage(self):
         with self._lock:
             return self._image
@@ -118,29 +105,8 @@ class DeepRacerCamera_impl(object):
         self._imagestream = value
         # Set the PipeConnecCallback to ImageStream_pipeconnect that will
         # called when a PipeEndpoint connects
-        value.PipeConnectCallback = self.ImageStream_pipeconnect
+        self._imagestream_broadcaster=RR.PipeBroadcaster(value,10)
     
-    def ImageStream_pipeconnect(self, pipe_ep):
-        # Lock the _imagestream_endpoints deictionary and place he pipe_ep in 
-        # a nested dict that is indexed by the endpoint of the client and the 
-        # index of the pipe
-        with self._imagestream_endpoints_lock:
-            # if there is not an enry for this client endpoint, add it
-            if (not pipe_ep.Endpoint in self._imagestream_endpoints):
-                self._imagestream_endpoints[pipe_ep.Endpoint] = dict()
-            
-            # Add pipe_ep to the correct dictionary given the endpoint + index
-            dict_ep = self._imagestream_endpoints[pipe_ep.Endpoint]
-            dict_ep[pipe_ep.Index] = pipe_ep
-            pipe_ep.PipeEndpointClosedCallback = self.ImageStream_pipeclosed
-    
-    def ImageStream_pipeclosed(self, pipe_ep):
-        with self._imagestream_endpoints_lock:
-            try:
-                dict_ep = self._imagestream_endpoints[pipe_ep.Endpoint]
-                del(dict_ep[pipe_ep.Index])
-            except:
-                return
 
     
 def main():
