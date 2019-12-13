f30d2020a2b9a477e9847fa771cf494bf1b58bad
John Parent <parenj@rpi.edu>
2019-12-13 15:17:58 -0500

    stream testing

---
+++ testing/getImageLoopTest.py
@@ -0,0 +1,33 @@
+from RobotRaconteur.Client import *
+import time
+import numpy as np
+import cv2
+import sys
+sys.path.append('..')
+import threading
+import logging
+
+
+def nd_arr_transform(ros_frame):
+    _shape = (ros_frame.height,ros_frame.width,3)
+    _dtype = np.uint8
+    _buffer = ros_frame.data
+    _offset = ros_frame.step
+    _order = 'C'
+    return np.ndarray(_shape,_dtype,_buffer,order=_order)
+
+
+if __name__ == '__main__':
+   
+    url_camera = 'rr+tcp://localhost:'+sys.argv[1]+'/?service=AWSCamera'
+    cam_data = RRN.ConnectService(url_camera)
+    raw_input("press enter to begin: ")
+    cam_data.startCamera()
+    while True:
+        im=cam_data.getCurrentImage()
+        im_ = nd_arr_transform(im)
+        cv2.imshow("Image Stream",im_)
+        if cv2.waitKey(50)!=-1:
+            break
+    cv2.destroyAllWindows()
+    
