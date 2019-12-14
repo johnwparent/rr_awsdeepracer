6538f0efba8551eb03f59aef9d67841d64fe5259
John Parent <parenj@rpi.edu>
2019-12-14 11:10:29 -0500

    streaming driving test

--- clients/laneDriver.py
+++ clients/laneDriver.py
@@ -74,6 +74,7 @@ class LaneDrive(object):
         self._obj = False
         self._servo = servo_ctrl
         self.c_drive_by_angle = 0.0
+        self._last_angle = None
     def drive(self):
         if len(self._lane_lines) == 0:
             logging.error('No lane lines detected, nothing to do.')
@@ -83,9 +84,10 @@ class LaneDrive(object):
         drive_by_angle = stabilize_steering_angle(self.c_drive_by_angle, new_steering_angle, len(self._lane_lines))/33.33
         self.c_drive_by_angle = drive_by_angle
         if self._servo is not None:
+            if not self._last_angle is None and abs(self._last_angle-self.c_drive_by_angle)>0.05:
 
-            self._servo.Drive(0.8,self.c_drive_by_angle)
-            self._servo.Drive(0.4,self.c_drive_by_angle)
+                
+                self._servo.Drive(0.4,self.c_drive_by_angle)
         
 
 
