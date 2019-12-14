5dbdb2ce6901de65c45720868dc4035ea7e7d07c
John Parent <parenj@rpi.edu>
2019-12-14 12:59:10 -0500

    streaming driving test

--- clients/laneDriver.py
+++ clients/laneDriver.py
@@ -16,32 +16,34 @@ def compute_steering_angle(frame, lane_lines):
         We assume that camera is calibrated to point to dead center
     """
     if len(lane_lines) == 0:
-        logging.info('No lane lines detected, do nothing')
         return 0
 
     height, width, _ = frame.shape
     if len(lane_lines) == 1:
-        logging.debug('Only detected one lane line, just follow it. %s' % lane_lines[0])
         x1, _, x2, _ = lane_lines[0][0]
         x_offset = width - x2
+
+        #ensures that with one lane line, we turn the correct direction
+        if x2 < (0.5*width):
+            x_offset = 0-abs(x_offset)
+        if x2 > (0.5*width):
+            x_offset = 0+abs(x_offset)
     else:
         _, _, left_x2, _ = lane_lines[0][0]
         _, _, right_x2, _ = lane_lines[1][0]
         half_width = 0.5 * width
         x_offset = half_width - (left_x2 + right_x2) / 2
 
-    # find the steering angle, which is angle between navigation direction to end of center line
     y_offset = int(height / 2)
 
-    angle_to_mid_radian = math.atan(x_offset / y_offset)  # angle (in radian) to center vertical line
-    angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)  # angle (in degrees) to center vertical line
-    steering_angle = angle_to_mid_deg  # this is the steering angle needed by picar front wheel
+    angle_to_mid_radian = math.atan(x_offset / y_offset) 
+    angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)  
+    steering_angle = angle_to_mid_deg 
 
-    logging.debug('new steering angle: %s' % steering_angle)
     return steering_angle
 
 
-    def length_of_line_segment(line):
+def length_of_line_segment(line):
         x1, y1, x2, y2 = line
         return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
@@ -64,7 +66,6 @@ def stabilize_steering_angle(curr_steering_angle, new_steering_angle, num_of_lan
                                         + max_angle_deviation * angle_deviation / abs(angle_deviation))
     else:
         stabilized_steering_angle = new_steering_angle
-    logging.info('Proposed angle: %s, stabilized angle: %s' % (new_steering_angle, stabilized_steering_angle))
     return stabilized_steering_angle
 
 class LaneDrive(object):
@@ -81,7 +82,7 @@ class LaneDrive(object):
 
         new_steering_angle = compute_steering_angle(self._frame, self._lane_lines)
         drive_by_angle = stabilize_steering_angle(self.c_drive_by_angle, new_steering_angle, len(self._lane_lines))
-        self.c_drive_by_angle = (drive_by_angle/33.333333)
+        self.c_drive_by_angle = (drive_by_angle/33.3333333333)
         if self._servo is not None:
 
                 
